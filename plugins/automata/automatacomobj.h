fbef9e82aaf5b99d84b75f2caf503741de1a0859
Nicu Tofan <nicu.tofan@gmail.com>
2017-03-10 18:14:49 +0300

    Fixes #1

--- plugins/automata/automatacomobj.h
+++ plugins/automata/automatacomobj.h
@@ -41,127 +41,127 @@ namespace	PlugIns		{
 /**
 *	@brief	Communication object used by the Automata_PlugIn class
 */
-class AutomataComObj	: private QObject			{
-	Q_OBJECT
+class AutomataComObj	: public QObject			{
+    Q_OBJECT
 
-	//
-	//
-	//
-	//
-	/*  DEFINITIONS    ----------------------------------------------------- */
+    //
+    //
+    //
+    //
+    /*  DEFINITIONS    ----------------------------------------------------- */
 
-	friend class Automata_PlugIn;
+    friend class Automata_PlugIn;
 
-	/*  DEFINITIONS    ===================================================== */
-	//
-	//
-	//
-	//
-	/*  DATA    ------------------------------------------------------------ */
+    /*  DEFINITIONS    ===================================================== */
+    //
+    //
+    //
+    //
+    /*  DATA    ------------------------------------------------------------ */
 
 private:
 
 
-	/**
-	*	@brief	associated plug-in
-	*/
-	Automata_PlugIn *		parent_;
+    /**
+    *	@brief	associated plug-in
+    */
+    Automata_PlugIn *		parent_;
 
 
-	/*  DATA    ============================================================ */
-	//
-	//
-	//
-	//
-	/*  FUNCTIONS    ------------------------------------------------------- */
+    /*  DATA    ============================================================ */
+    //
+    //
+    //
+    //
+    /*  FUNCTIONS    ------------------------------------------------------- */
 
 public:
 
 
-	/**
-	*	@brief	constructor;
-	*/
-	AutomataComObj			( Automata_PlugIn * p );
+    /**
+    *	@brief	constructor;
+    */
+    AutomataComObj			( Automata_PlugIn * p );
 
 
-	/**
-	*	@brief	destructor;
-	*/
-	~AutomataComObj			( void );
+    /**
+    *	@brief	destructor;
+    */
+    ~AutomataComObj			( void );
 
 
 public slots:
 
 
-	/**
-	*	@brief	change the way command strings are interpretted (characters / strings)
-	*/
-	void			setDefaultInterpMode	( bool md )
-	{ parent_->setDefaultInterpMode(
-					( md ?
-						  Automata_PlugIn::IMP_STRINGS :
-						  Automata_PlugIn::IMP_CHARACTERS
-						  )
-					);
-	}
+    /**
+    *	@brief	change the way command strings are interpretted (characters / strings)
+    */
+    void			setDefaultInterpMode	( bool md )
+    { parent_->setDefaultInterpMode(
+                    ( md ?
+                          Automata_PlugIn::IMP_STRINGS :
+                          Automata_PlugIn::IMP_CHARACTERS
+                          )
+                    );
+    }
 
 
-	/**
-	*	@brief	stops the execution of the program
-	*/
-	void			stopAutomProg			( void )
-	{ parent_->stopAutomProg(); }
+    /**
+    *	@brief	stops the execution of the program
+    */
+    void			stopAutomProg			( void )
+    { parent_->stopAutomProg(); }
 
 
-	/**
-	*	@brief	strarts the execution of the program
-	*/
-	void			runAutomProg			( void )
-	{ parent_->runAutomProg(); }
+    /**
+    *	@brief	strarts the execution of the program
+    */
+    void			runAutomProg			( void )
+    { parent_->runAutomProg(); }
 
 
-	/**
-	*	@brief	strarts the execution of the program in step mode
-	*/
-	void			stepAutomProg			( void )
-	{ parent_->stepAutomProg(); }
+    /**
+    *	@brief	strarts the execution of the program in step mode
+    */
+    void			stepAutomProg			( void )
+    { parent_->stepAutomProg(); }
 
 
-	/**
-	*	@brief	informed when the text has changed
-	*/
-	void			editorChanged			( void )
-	{ parent_->editorChanged(); }
+    /**
+    *	@brief	informed when the text has changed
+    */
+    void			editorChanged			( void )
+    { parent_->editorChanged(); }
 
 
-	/**
-	*	@brief	tiggered when the timeout expires on run timer
-	*/
-	void			timeoutRun				( void )
-	{ parent_->performSteps( 0 ); }
+    /**
+    *	@brief	tiggered when the timeout expires on run timer
+    */
+    void			timeoutRun				( void )
+    { parent_->performSteps( 0 ); }
 
 
-	/**
-	*	@brief	informed about the close of toolbar
-	*/
-	void			tbClosing				( void )
-	{ parent_->noTb(); }
+    /**
+    *	@brief	informed about the close of toolbar
+    */
+    void			tbClosing				( void )
+    { parent_->noTb(); }
 
 
-	/**
-	*	@brief	informed about the close of tool-window
-	*/
-	void			twClosing				( void )
-	{ parent_->noTw(); }
+    /**
+    *	@brief	informed about the close of tool-window
+    */
+    void			twClosing				( void )
+    { parent_->noTw(); }
 
 
 
 
-	/*  FUNCTIONS    ======================================================= */
-	//
-	//
-	//
-	//
+    /*  FUNCTIONS    ======================================================= */
+    //
+    //
+    //
+    //
 
 
 };	/*	class AutomataComObj	*/
